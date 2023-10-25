14c37fe3b0f663c7b7481207d49a142d993ef601
cgadal <cgadal@users.noreply.github.com>
2023-10-25 09:47:08 +0000

    deploy: 0aea2f04bef850dd03c555847ebc7f7abae2e19e

---
+++ _downloads/fc7f943e02a85cafc64e7439da4d29e2/download_ERAdata.py
@@ -0,0 +1,29 @@
+"""
+=======================================
+Downloading data from the ERA* datasets
+=======================================
+
+Please find below a typical script allowing to download data from the ERA5 Land dataset.
+"""
+
+from pydune.data_processing import getting_CDSdata
+
+month = [i for i in range(1, 2)]
+day = [i for i in range(1, 20)]
+time = [i for i in range(0, 24)]
+year = [i for i in range(2000, 2001)]
+area = [-16.65, 11.9, -16.66, 11.91]
+
+variable_dic = {'format': 'netcdf',
+                'variable': ['10m_u_component_of_wind', '2m_temperature'],
+                'month': month,
+                'day': day,
+                'time': time,
+                'year': year,
+                'area': area}
+
+a = getting_CDSdata('reanalysis-era5-single-levels',
+                     variable_dic, 'Angola_coast_v10',
+                     Nsplit=1,
+                     file='info.txt',
+                     on_grid=False)
